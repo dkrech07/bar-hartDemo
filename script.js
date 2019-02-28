9b163a80ea8a4fc6fee48b42298ffecd55e54050
Дмитрий Кречетников <dkrech07@gmail.com>
2019-02-28 18:57:31 +0500

    second commit

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ for (var i = 0; i < data.length; i++) {
   var dp = data[i];
   c.fillRect(25 + i * 100, 500 - dp * 5 - 30, 50, dp * 5);
 }
+
+// рисуем осевые линии
+c.fillStyle = "black";
+c.lineWidth = 2.0;
+c.beginPath();
+c.moveTo(30, 10);
+c.lineTo(30, 460);
+c.lineTo(490, 460);
+c.stroke();
+
+// рисуем текст и вертикальные линии
+c.fillStyle = "black";
+for (var i = 0; i < 6; i++) {
+  c.fillText((5 - i) * 20 + "", 4, i * 80 + 60);
+  c.beginPath();
+  c.moveTo(25, i * 80 + 60);
+  c.lineTo(30, i * 80 + 60);
+  c.stroke();
+}
+
+var labels = ["JAN", "FEB", "MAR", "APR", "MAY"];
+// выводим контекст
+for (var i = 0; i < 5; i++) {
+  c.fillText(labels[i], 50 + i * 100, 475);
+}
